74ae91f085a53d17edd62b1331f8dfdb1422d28b
Juan Diaz <juanchodis@hotmail.com>
2023-11-29 22:32:47 -0500

    [8] docs.jpdiaz.dev - E-commerce Site with React.js, Vite.js and Tailwind CSS - 8. Consume an API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,9 +1,10 @@
-const Card = () => {
+const Card = (data) => {
   return (
     <div className="bg-amber-600/80 cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 bg-white/60 rounded-lg text-black text-xs m-2 py-0.5 px-2">
-          tag
+          {/* {console.log(data)}
+          {console.log("id: ", data.data.id)} */}
         </span>
         <img
           className="rounded-lg w-full h-full object-cover"
@@ -11,8 +12,7 @@ const Card = () => {
           alt="card image"
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white rounded-full w-6 h-6 m-2">
-          {" "}
-          +{" "}
+          +
         </div>
       </figure>
       <p className="flex justify-around">
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from "react"; // Hooks to consume the API
 import Card from "../../Components/Card";
 import Layout from "../../Components/Layout";
 
 const Home = () => {
+  // UseState is a hook to add the info from the API to the state
+  const [items, setItems] = useState("");
+
+  // UseEffect is a hook to fetch the data from the API
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((json) => setItems(json));
+    // .then((json) => console.log(json));
+  }, []);
+
   return (
     <Layout>
-      <Card />
+      <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {console.log("items: ", items)}
+        {/* {console.log("products: ", items.products)}
+        {console.log("id: ", items.products.id)}
+        {console.log("price: ", items.products.price)} */}
+
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+        {/* {container &&
+              container.map((item) => <Card key={item.id} data={item} />)} */}
+      </div>
     </Layout>
   );
 };
