53af83b4f04341057831aee03d4dca722401b615
Juan Diaz <juanchodis@hotmail.com>
2023-11-30 06:06:51 -0500

    [8] docs.jpdiaz.dev - E-commerce Site with React.js, Vite.js and Tailwind CSS - 8. Consume an API

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,23 +1,27 @@
 const Card = (data) => {
+  {
+    console.log("Data: ", data.data);
+    console.log("Data.data: ", data.data);
+    console.log("id: ", data.data.id);
+  }
   return (
     <div className="bg-amber-600/80 cursor-pointer w-56 h-60 rounded-lg">
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 bg-white/60 rounded-lg text-black text-xs m-2 py-0.5 px-2">
-          {/* {console.log(data)}
-          {console.log("id: ", data.data.id)} */}
+          {data.data.category}
         </span>
         <img
           className="rounded-lg w-full h-full object-cover"
-          src="https://images.pexels.com/photos/3861969/pexels-photo-3861969.jpeg?auto=compress&cs=tinysrgb&w=300"
-          alt="card image"
+          src={data.data.images[0]}
+          alt={data.data.title}
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white rounded-full w-6 h-6 m-2">
           +
         </div>
       </figure>
       <p className="flex justify-around">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-lg font-medium">$300</span>
+        <span className="text-sm font-light">{data.data.title}</span>
+        <span className="text-lg font-medium">${data.data.price}</span>
       </p>
     </div>
   );
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -4,29 +4,30 @@ import Layout from "../../Components/Layout";
 
 const Home = () => {
   // UseState is a hook to add the info from the API to the state
-  const [items, setItems] = useState("");
+  const [items, setItems] = useState([]); // State to store the data from the dummy API. It's an empty array because the data is an array of objects
 
   // UseEffect is a hook to fetch the data from the API
   useEffect(() => {
     fetch("https://dummyjson.com/products")
       .then((response) => response.json())
-      .then((json) => setItems(json));
-    // .then((json) => console.log(json));
+      .then((json) => {
+        console.log("Data from Dummy API: ", json); // Log the data
+        setItems(json.products); // Add the data to the state (setItems) and specify the data to be added (json.products)
+      });
   }, []);
 
   return (
     <Layout>
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {console.log("items: ", items)}
+        {/* {console.log("Set Items: ", setItems)} */}
+        {/* {console.log("items: ", items)} */}
         {/* {console.log("products: ", items.products)}
-        {console.log("id: ", items.products.id)}
-        {console.log("price: ", items.products.price)} */}
+        {console.log("id: ", items.products.id)} */}
+        {/* {console.log("price: ", items.products.price)} */}
 
         {items?.map((item) => (
           <Card key={item.id} data={item} />
         ))}
-        {/* {container &&
-              container.map((item) => <Card key={item.id} data={item} />)} */}
       </div>
     </Layout>
   );
