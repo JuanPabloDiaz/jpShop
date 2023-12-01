6bd739e91fa882a83c5f2f38396c6b53623115a0
Juan Diaz <juanchodis@hotmail.com>
2023-12-01 07:09:40 -0500

    [27] docs.jpdiaz.dev - E-commerce Site with React.js, Vite.js and Tailwind CSS - 27. Refactor of my Home Page

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,8 +1,23 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+  // Home · Get Products - Fetch data from API
+  // UseState is a hook to add the info from the API to the state
+  const [items, setItems] = useState([]); // State to store the data from the dummy API. It's an empty array because the data is an array of objects
+
+  // UseEffect is a hook to fetch the data from the API
+  useEffect(() => {
+    fetch("https://dummyjson.com/products")
+      .then((response) => response.json())
+      .then((json) => {
+        // console.log("Data from Dummy API: ", json); // Log the data
+        // console.log("Products inside Data Dummy API: ", json.products); // Products is an array of objects inside the data from the API
+        setItems(json.products); // Add the data to the state (setItems) and specify the data to be added (json.products)
+      });
+  }, []);
+
   // Shopping Cart · Increment quantity
   const [count, setCount] = useState(0);
 
@@ -28,6 +43,8 @@ export const AppProvider = ({ children }) => {
   return (
     <AppContext.Provider
       value={{
+        items,
+        setItems,
         count,
         setCount,
         openProductDetail,
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,28 +1,20 @@
-import { useState, useEffect } from "react"; // Hooks to consume the API
+import { useContext } from "react";
 import Card from "../../Components/Card";
 import Layout from "../../Components/Layout";
 import ProductDetail from "../../Components/ProductDetail";
+import { AppContext } from "../../Context";
 
 const Home = () => {
-  // UseState is a hook to add the info from the API to the state
-  const [items, setItems] = useState([]); // State to store the data from the dummy API. It's an empty array because the data is an array of objects
-
-  // UseEffect is a hook to fetch the data from the API
-  useEffect(() => {
-    fetch("https://dummyjson.com/products")
-      .then((response) => response.json())
-      .then((json) => {
-        // console.log("Data from Dummy API: ", json); // Log the data
-        setItems(json.products); // Add the data to the state (setItems) and specify the data to be added (json.products)
-      });
-  }, []);
-
-  // console.log("items: ", items); // Log the state
-
+  const context = useContext(AppContext);
+  // {
+  //   console.log("Context: ", context);
+  //   console.log("items: ", context.items);
+  // }
   return (
     <Layout>
+      <h1>Home</h1>
       <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
-        {items?.map((item) => (
+        {context.items?.map((item) => (
           <Card key={item.id} data={item} />
         ))}
       </div>
