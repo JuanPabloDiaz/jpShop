bf94dcd3375e8ca88127efc01ada69d7b8885663
Juan Diaz <juanchodis@hotmail.com>
2023-12-01 15:27:27 -0500

    32. Improve the UI - styling the site - card moves when hover

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -46,7 +46,7 @@ const Card = (data) => {
 
   return (
     <div
-      className="bg-white border shadow-lg cursor-pointer w-56 h-60 rounded-lg"
+      className="bg-white border shadow-lg cursor-pointer w-56 h-60 rounded-lg  hover:scale-105 transition duration-300"
       onClick={() => showProduct(data.data)}
     >
       <figure className="relative mb-2 w-full h-4/5">
